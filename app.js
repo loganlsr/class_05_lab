b5c4e6738ab3415bd6ec693febc1027205f43436
Logan Rogers <logan.s.rogers@biola.edu>
2016-06-17 14:39:23 -0700

    Done with question 2

--- app.js
+++ app.js
@@ -26,8 +26,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){
-
+  var result = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + result + '.';
+  return [result, message];
 }
+
+testMultiply(4,7);
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(4,7);
